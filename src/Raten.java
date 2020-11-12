c0d837b4af73c5c90504f79726b1507d64cfeb02
Nils <nils.weisser@rhs-bs.de>
2020-11-12 22:30:58 +0100

    test

--- src/Raten.java
+++ src/Raten.java
@@ -1,19 +1,2 @@
-public class Raten{
-    public static void main(String[] args) {
-        if(args.length > 0) {
-            computerRaten(Integer.parseInt(args[0]));
-        }
-        }
-
-    private static void computerRaten(int arg) {
-        int count = 1;
-        while(true) {
-            int randomint = (int) (Math.random() * 100);
-            if (randomint == arg) {
-                System.out.println(""+count);
-                break;
-            }
-            count++;
-        }
-        }
+public class Raten {
 }
