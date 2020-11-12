216b9648ed3b3e8efa8e08d074c2f2ca9fdaeb3c
Nils <nils.weisser@rhs-bs.de>
2020-11-12 22:26:34 +0100

    Raten2

--- src/Raten.java
+++ src/Raten.java
@@ -1,19 +1,29 @@
+import java.util.Scanner;
+
 public class Raten{
     public static void main(String[] args) {
-        if(args.length > 0) {
-            computerRaten(Integer.parseInt(args[0]));
+        if (args.length == 0) {
+            System.out.println("Ok");
+            selberRaten();
+        }
+    }
+
+    private static void selberRaten() {
+        int zufallszahl = (int) (Math.random()*100);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int nextNumber = sc.nextInt();
+            if(nextNumber == zufallszahl) {
+                System.out.println("Geschafft!");
+                break;
+            }
+            else if(nextNumber < zufallszahl) {
+                System.out.println("Die eingebene Zahl ist kleiner als die gesuchte Zufallszahl");
+            }
+            else {
+                System.out.println("Die eingebene Zahl ist groesser als die gesuchte Zufallszahl");
+            }
         }
         }
 
-    private static void computerRaten(int arg) {
-        int count = 1;
-        while(true) {
-            int randomint = (int) (Math.random() * 100);
-            if (randomint == arg) {
-                System.out.println(""+count);
-                break;
-            }
-            count++;
-        }
-        }
 }
